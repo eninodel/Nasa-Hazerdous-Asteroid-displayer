cac497bd2929bc919c7e15229211fd01f44dc4a5
Edwin Nino Delgado <71790814+eninodel@users.noreply.github.com>
2021-02-15 22:46:39 -0800

    Update Nasa.py

--- Nasa.py
+++ Nasa.py
@@ -9,7 +9,7 @@ API_KEY = ""
 
 This program accesses NASA's public API to obtain daily information about
 potential dangerous asteroids and information about them such as name, 
-velocity, and miles from Earth. This program then send the information to 
+velocity, and miles from Earth. This program then sends the information to 
 an LCD screen attached to a Raspberry Pi.
 
 """
